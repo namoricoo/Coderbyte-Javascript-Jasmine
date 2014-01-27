96a61388f558c59b5b878d4c7110439f65d261ac
Nate <namoricoo@hotmail.com>
2014-01-26 23:36:12 -0800

    remove all jslint errors for firstReverse.js

--- src/firstReverse.js
+++ src/firstReverse.js
@@ -1,11 +1,10 @@
 var firstReverse = function(stringInput) {
-         "use strict";
-         var characterArray = stringInput.split('');
-         var outputString = '';
-         var index;
-         for(index=characterArray.length - 1;index > -1; index -= 1)
-         {
-         	outputString += characterArray[index];         	
-         }
-         return outputString;
+        "use strict";
+        var characterArray = stringInput.split(''),
+            outputString = '',
+            index;
+        for (index = characterArray.length - 1; index > -1; index -= 1) {
+            outputString += characterArray[index];
+        }
+        return outputString;
     };
