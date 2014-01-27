1fc76232758a0ad047bd5658d6724412e4f878e6
Nate <namoricoo@hotmail.com>
2014-01-27 00:36:20 -0800

    refactored firstReverse.js code is now lint free

--- src/firstReverse.js
+++ src/firstReverse.js
@@ -3,9 +3,9 @@ var firstReverse = function(stringInput) {
         var resultString = '',
             size = stringInput.length,
             index;
-            for(index = 0; index < size; index += 1) {
-            	resultString += stringInput.charAt(size - index);
-            }
-            resultString += stringInput.charAt(0);
-            return resultString;       
+        for (index = 0; index < size; index += 1) {
+            resultString += stringInput.charAt(size - index);
+        }
+        resultString += stringInput.charAt(0);
+        return resultString;
     };
