f443ee3bd1cf0b1946c3fcce39018fa2b4acb8e8
Nate <namoricoo@hotmail.com>
2014-01-27 09:57:19 -0800

    firstReverse spec two test

--- spec/firstReverse_spec.js
+++ spec/firstReverse_spec.js
@@ -4,5 +4,10 @@ describe("firstReverse", function() {
          var stringInput = 'coderbyte';
          var stringOutput = 'etybredoc';
          expect(firstReverse(stringInput)).toEqual(stringOutput);
-     });	
+     });
+     it("reverses a string Input = 'coderbyte' Output = 'etybredoc' ", function() {
+         var stringInput = 'I Love Code';
+         var stringOutput = 'edoC evoL I';
+         expect(firstReverse(stringInput)).toEqual(stringOutput);
+     });
 });
