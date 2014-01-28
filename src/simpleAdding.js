15c528d9de68dc5314bbeaea2dcabd789e3d9cd8
Nate <namoricoo@hotmail.com>
2014-01-28 10:38:33 -0800

    refactor simpleAdding.js to comply with Jslint

--- src/simpleAdding.js
+++ src/simpleAdding.js
@@ -1,18 +1,18 @@
+function isDigit(input) {
+    'use strict';
+    return (/^\d+$/.test(input));
+}
+
 var simpleAdding = function(upperIntLimit) {
         'use strict';
         var total = 0,
             index;
-        if(isDigit(upperIntLimit)) {
-          for(index = 0; index <= upperIntLimit; index += 1) {
-            total += index;
-          }
-        }
-        else {
-          return 'error';
+        if (isDigit(upperIntLimit)) {
+            for (index = 0; index <= upperIntLimit; index += 1) {
+                total += index;
+            }
+        } else {
+            return 'error';
         }
         return total;
     };
-function isDigit(input)
-{
-  return /^\d+$/.test(input);
-}  
