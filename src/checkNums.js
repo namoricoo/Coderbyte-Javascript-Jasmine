f9b1f4348566c0081e6592e50f2bffd08c979d04
Nate <namoricoo@hotmail.com>
2014-01-28 22:40:32 -0800

    pass test for checkNums

---
+++ src/checkNums.js
@@ -0,0 +1,8 @@
+var checkNums = function (numberOne, numberTwo) {
+        'use strict';
+        if (numberTwo > numberOne) {
+          return 'true';
+        } else {
+          return '-1';
+        }
+    };
