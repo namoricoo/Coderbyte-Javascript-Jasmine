636db23c7129fe1f32557362cb39891e268ee3c1
Nate <namoricoo@hotmail.com>
2014-01-28 17:24:18 -0800

    resolve Jslint problems in simpleSymbol.js

--- src/simpleSymbol.js
+++ src/simpleSymbol.js
@@ -1,25 +1,26 @@
 function plusCharPlusPattern(inputString, index) {
-var previousChar = 	inputString[index - 1],
-    currentChar = inputString[index],
-    nextChar = inputString[index + 1],
-    patternString = previousChar + currentChar + nextChar;
+    'use strict';
+    var previousChar = inputString[index - 1],
+        currentChar = inputString[index],
+        nextChar = inputString[index + 1],
+        patternString = previousChar + currentChar + nextChar;
     return (/^[+][A-z][+]/.test(patternString));
 }
 
 var simpleSymbol = function(inputString) {
         'use strict';
-        var nextCharacter = '',
-            currentCharacter = '',
+        var currentCharacter = '',
             output = false,
             index;
         for (index = 0; index < inputString.length; index += 1) {
             currentCharacter = inputString[index];
-            if (/^[A-z]+$/.test(currentCharacter)) {
-              if (plusCharPlusPattern(inputString, index)) {
-              	output = true;
-              }
-              else return false;
-            }            
+            if (/[A-z]/.test(currentCharacter)) {
+                if (plusCharPlusPattern(inputString, index)) {
+                    output = true;
+                } else {
+                    return false;
+                }
+            }
         }
         return output;
     };
