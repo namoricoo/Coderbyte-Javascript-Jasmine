e1cb666f7fd73e5bca64b88f577fcaccdd0e6a8e
Nate <namoricoo@hotmail.com>
2014-01-28 22:09:22 -0800

    add second test to simpleSymbol_spec

--- spec/simpleSymbol_spec.js
+++ spec/simpleSymbol_spec.js
@@ -9,9 +9,9 @@ describe('simpleSymbol', function() {
             output = true;
         expect(simpleSymbol(input)).toEqual(output);
     });
-    it(" Input = '+d+=3=+s+' Output = 'true' ", function() {
-        var input = '+d+=3=+s+',
-            output = true;
+    it(" Input = 'f++d+' Output = 'false' ", function() {
+        var input = 'f++d+',
+            output = false;
         expect(simpleSymbol(input)).toEqual(output);
     });
 });
