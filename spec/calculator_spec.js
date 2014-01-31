6d70c593d3a8988a39dd8e7dc7833be475ede4c0
Nate <namoricoo@hotmail.com>
2014-01-30 22:35:53 -0800

    test for adding a subsequent number

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -3,5 +3,8 @@ describe("Calculator", function() {
     var input = 5,
         output = 5;
     expect(Calculator.add(input)).toEqual(output);
+    // See what happens if we add a second number
+        output = 10;
+    expect(Calculator.add(input)).toEqual(output);
   });
 });
