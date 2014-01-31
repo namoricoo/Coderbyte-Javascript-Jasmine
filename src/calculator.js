58fc6cc9692866a1a1055b8ec3ea408d8327abd8
Nate <namoricoo@hotmail.com>
2014-01-30 23:51:23 -0800

    add method is now able to take multiple numbers as inputs

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,12 @@
 window.Calculator = {
   current: 0,
-  add: function(inputFloat) {
-    this.current += inputFloat;
+  add: function(inputArray) {
+    var sum = this.current;
+    var i;
+    for(i = 0, len = arguments.length; i< len; i++) {
+      sum += arguments[i];
+    }
+    this.current = sum;
     return this.current;
   }
 };
