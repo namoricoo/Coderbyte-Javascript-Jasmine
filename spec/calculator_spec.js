da4e54e8ad243f0b938900250cdb111ef9a797ed
Nate <namoricoo@hotmail.com>
2014-01-30 23:23:10 -0800

    using the before each statement to reset the calculator back to zero before we call any new it statements

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -1,4 +1,8 @@
 describe("Calculator", function() {
+  beforeEach(function() {
+    Calculator.current = 0;
+  });
+
   it("should store the current value at all times", function() {
     expect(Calculator.current).toBeDefined();    
   });
@@ -14,5 +18,9 @@ describe("Calculator", function() {
     // See what happens if we add a second number
     output = 10;
     expect(Calculator.add(input)).toEqual(output);
+  });
+  it("should add any number of numbers", function() {
+      var output = 6;
+      expect(Calculator.add(1,2,3)).toEqual(output);
   });
 });
